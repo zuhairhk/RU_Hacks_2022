dec291a3350fb7b17702099963ff81389128e15f
zuhairhk <zuhairhk@gmail.com>
2022-05-06 20:03:18 -0400

    Initial Setup Complete

--- main.py
+++ main.py
@@ -9,4 +9,10 @@ load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
 intents = Intents.all()
-bot = commands.Bot(command_prefix = "$", intents = intents)
+bot = commands.Bot(command_prefix = "$") #, intents = intents)
+
+@bot.event
+async def on_ready():
+    print('Ready!')
+
+bot.run(TOKEN)
