fab97e060b3e71f198bc28113973e20f18030797
zuhairhk <zuhairhk@gmail.com>
2022-05-07 01:40:55 -0400

    Finished Bulk of Light Controller

--- cogs/light_controller.py
+++ cogs/light_controller.py
@@ -1,3 +1,4 @@
+# Import Statments
 from nextcord.ext import commands
 from nextcord import Client
 from tuya_connector import TuyaOpenAPI
@@ -20,19 +21,44 @@ class LightController(commands.Cog):
 
     @commands.command()
     async def light_state(self, ctx, mode):
+        state(mode=mode)
+    
+    @commands.command()
+    async def set_colour(self, ctx, colour):
+        colourSet(colour=colour)
+    
+    @commands.command()
+    async def set_mode(self, ctx, mode):
+        colourSet(mode=mode)
+
+# Cog Setup Function
+def setup(bot):
+    bot.add_cog(LightController(bot))
+
+# 
+def state(mode):
         if mode.lower() == 'on':
             state = True
         elif mode.lower() == 'off':
             state = False
         commands = {'commands': [{'code': 'switch_led', 'value': state}]}
         openapi.post(f'/v1.0/iot-03/devices/{LIGHTBULB_DEVICE_ID}/commands', commands)
-        await ctx.send(f'Light is now {mode}')
-    
-    @commands.command()
-    async def set_colour(self, ctx, colour):
-        commands = {'commands': [{ 'code': 'colour_data_v2', 'value': {'h': colour, 's': 1000, 'v': 1000} }]}
-        openapi.post(f'/v1.0/iot-03/devices/{LIGHTBULB_DEVICE_ID}/commands', commands)
-        await ctx.send(f'Light changed to {colour}')
 
-def setup(bot):
-    bot.add_cog(LightController(bot))
+# 
+def colourSet(colour):
+    colors = {
+            'red' : 0,
+            'orange' : 30,
+            'yellow' : 45,
+            'green' : 120,
+            'cyan' : 180,
+            'blue' : 245,
+            'pink' : 300
+        }
+    commands = {'commands': [{ 'code': 'colour_data_v2', 'value': {'h': colors[colour], 's': 1000, 'v': 1000} }]}
+    openapi.post(f'/v1.0/iot-03/devices/{LIGHTBULB_DEVICE_ID}/commands', commands)
+
+# 
+def lightMode(mode):
+    commands = {'commands': [{'code': 'work_mode', 'value': mode}] }
+    openapi.post(f'/v1.0/iot-03/devices/{LIGHTBULB_DEVICE_ID}/commands', commands)
--- main.py
+++ main.py
@@ -3,7 +3,10 @@ import os
 from dotenv import load_dotenv
 import nextcord
 from nextcord.ext import commands
-from nextcord import Intents
+from nextcord.abc import GuildChannel
+from nextcord import Intents, Interaction, SlashOption, Member, ChannelType
+
+from cogs import light_controller
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -14,6 +17,36 @@ bot = commands.Bot(command_prefix = "$", intents = intents)
 @bot.event
 async def on_ready():
     print('Ready!')
+
+# Light On/Off Slash Command
+@bot.slash_command(
+    name="light",
+    description="State of Bulb (On/Off)",
+    guild_ids=[793978686822154240]
+)
+async def State(interaction: Interaction, mode):
+    light_controller.state(mode=mode)
+    await interaction.response.send_message(f"Light State Set To --> {mode}")
+
+# Light Colour Change Slash Command
+@bot.slash_command(
+    name= "colour_set",
+    description = "Red, Orange, Yellow, Green, Cyan, Blue, Pink",
+    guild_ids=[793978686822154240]
+)
+async def colour_set(interaction: Interaction, colour):
+    light_controller.colourSet(colour=colour)
+    await interaction.response.send_message(f"Light State Set To --> {colour}")
+
+# Light Mode Change Slash Command
+@bot.slash_command(
+    name= "mode",
+    description = "White, Colour, Scence, Music",
+    guild_ids=[793978686822154240]
+)
+async def mode(interaction: Interaction, mode):
+    light_controller.lightMode(mode=mode)
+    await interaction.response.send_message(f"Light State Set To --> {mode}")
 
 if __name__ == "__main__":
     for filename in os.listdir("./cogs"):
