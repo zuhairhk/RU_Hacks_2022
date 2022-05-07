b938eb98e55e1a7bc42b8abc5e9de53251d3f284
Nicholas Mascioni <68752011+nicholasmascioni@users.noreply.github.com>
2022-05-06 22:09:44 -0400

    Plan

---
+++ cogs/tic_tac_toe.py
@@ -0,0 +1,7 @@
+'''
+Create a 3x3 array of buttons, display output (initially empty 3x3 array) below buttons after each press
+Once a button is pressed, disable it (can't choose a space more than once)
+Once a user completes a row, column, or diagonal game is over
+Gotta implement some logic for unwinnable gamestates
+😵‍💫
+'''
