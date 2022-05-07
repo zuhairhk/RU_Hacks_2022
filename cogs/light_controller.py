1b298bb769b0a0a07696543f304280a885593036
zuhairhk <zuhairhk@gmail.com>
2022-05-07 12:08:20 -0400

    bringing up to date

---
+++ cogs/light_controller.py
@@ -0,0 +1,64 @@
+# Import Statments
+from nextcord.ext import commands
+from nextcord import Client
+from tuya_connector import TuyaOpenAPI
+import os
+
+# env variables
+ACCESS_ID = os.environ['ACCESS_ID']
+ACCESS_KEY = os.environ['ACCESS_KEY']
+API_ENDPOINT = os.environ['API_ENDPOINT']
+LIGHTBULB_DEVICE_ID = os.environ['LIGHTBULB_DEVICE_ID']
+
+# CONNECTION
+openapi = TuyaOpenAPI(API_ENDPOINT, ACCESS_ID, ACCESS_KEY)
+openapi.connect()
+
+class LightController(commands.Cog):
+    
+    def __init__(self, bot : Client):
+        self.bot = bot
+
+    @commands.command()
+    async def light_state(self, ctx, mode):
+        state(mode=mode)
+    
+    @commands.command()
+    async def set_colour(self, ctx, colour):
+        colourSet(colour=colour)
+    
+    @commands.command()
+    async def set_mode(self, ctx, mode):
+        colourSet(mode=mode)
+
+# Cog Setup Function
+def setup(bot):
+    bot.add_cog(LightController(bot))
+
+# State of Light Function
+def state(mode):
+        if mode.lower() == 'on':
+            state = True
+        elif mode.lower() == 'off':
+            state = False
+        commands = {'commands': [{'code': 'switch_led', 'value': state}]}
+        openapi.post(f'/v1.0/iot-03/devices/{LIGHTBULB_DEVICE_ID}/commands', commands)
+
+# Colour Set Function
+def colourSet(colour):
+    colors = {
+            'red' : 0,
+            'orange' : 30,
+            'yellow' : 45,
+            'green' : 120,
+            'cyan' : 180,
+            'blue' : 245,
+            'pink' : 300
+        }
+    commands = {'commands': [{ 'code': 'colour_data_v2', 'value': {'h': colors[colour], 's': 1000, 'v': 1000} }]}
+    openapi.post(f'/v1.0/iot-03/devices/{LIGHTBULB_DEVICE_ID}/commands', commands)
+
+# Light Mode Set Function
+def lightMode(mode):
+    commands = {'commands': [{'code': 'work_mode', 'value': mode}] }
+    openapi.post(f'/v1.0/iot-03/devices/{LIGHTBULB_DEVICE_ID}/commands', commands)
--- main.py
+++ main.py
@@ -3,10 +3,14 @@ import os
 from dotenv import load_dotenv
 import nextcord
 from nextcord.ext import commands
-from nextcord import Intents
+from nextcord.abc import GuildChannel
+from nextcord import Intents, Interaction, SlashOption, Member, ChannelType
+from cogs import light_controller, search_func
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+
+# Bot Invite Link: https://discord.com/api/oauth2/authorize?client_id=972281781564887052&permissions=8&scope=bot%20applications.commands
 
 intents = Intents.all()
 bot = commands.Bot(command_prefix = "$", intents = intents)
@@ -15,9 +19,49 @@ bot = commands.Bot(command_prefix = "$", intents = intents)
 async def on_ready():
     print('Ready!')
 
+# Light On/Off Slash Command
+@bot.slash_command(
+    name="light",
+    description="State of Bulb (On/Off)",
+    guild_ids=[793978686822154240]
+)
+async def State(interaction: Interaction, mode):
+    light_controller.state(mode=mode)
+    await interaction.response.send_message(f"Light State Set To --> {mode}")
+
+# Light Colour Change Slash Command
+@bot.slash_command(
+    name= "colour_set",
+    description = "Red, Orange, Yellow, Green, Cyan, Blue, Pink",
+    guild_ids=[793978686822154240]
+)
+async def colour_set(interaction: Interaction, colour):
+    light_controller.colourSet(colour=colour)
+    await interaction.response.send_message(f"Light State Set To --> {colour}")
+
+# Light Mode Change Slash Command
+@bot.slash_command(
+    name= "mode",
+    description = "White, Colour, Scence, Music",
+    guild_ids=[793978686822154240]
+)
+async def mode(interaction: Interaction, mode):
+    light_controller.lightMode(mode=mode)
+    await interaction.response.send_message(f"Light State Set To --> {mode}")
+
+# Google Search Slash Command
+@bot.slash_command(
+    name= "search",
+    description = "Enter Query",
+    guild_ids=[793978686822154240]
+)
+async def search(interaction: Interaction, query):
+    response = search_func.searcher(query=query)
+    await interaction.response.send_message(f"Google Response --> {response}")
+
 if __name__ == "__main__":
     for filename in os.listdir("./cogs"):
         if filename.endswith(".py"):
             bot.load_extension(f"cogs.{filename[:-3]}")
     
-bot.run(TOKEN)
+    bot.run(TOKEN)
