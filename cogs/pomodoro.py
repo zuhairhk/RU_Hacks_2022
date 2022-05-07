06b8a1e65ce90ee2ead09d4697a06aecf18b8826
zuhairhk <zuhairhk@gmail.com>
2022-05-07 03:47:43 -0400

    Fixed Pomodo Function Errors

--- cogs/pomodoro.py
+++ cogs/pomodoro.py
@@ -1,11 +1,11 @@
 from nextcord.ext import commands
 from nextcord import Client
 
-class GoogleScraper(commands.Cog):
+class PomodoroHandeler(commands.Cog):
     
     def __init__(self, bot : Client):
         self.bot = bot
 
 # Cog Setup Function
 def setup(bot):
-    bot.add_cog(GoogleScraper(bot))
+    bot.add_cog(PomodoroHandeler(bot))
